6e6f0840584610ed39f6f698d914c8a277876fcb
firdasafridi <firdasafridi@ymail.com>
2019-07-14 18:23:12 +0700

    Add env

---
+++ env.go
@@ -0,0 +1,37 @@
+package indodax
+
+import (
+	"os"
+)
+
+//
+// environment contains values that gathered from external
+// resources, for example system environment variables.
+//
+type environment struct {
+	BaseHostPublic  string
+	BaseHostPrivate string
+	apiKey          string
+	apiSecret       string
+}
+
+func newEnvironment() (env *environment) {
+	env = &environment{
+		BaseHostPublic:  UrlPublic,
+		BaseHostPrivate: UrlPrivate,
+		apiKey:          os.Getenv("INDODAX_KEY"),
+		apiSecret:       os.Getenv("INDODAX_SECRET"),
+	}
+
+	pubHost := os.Getenv("INDODAX_PUB_HOST")
+	if pubHost != "" {
+		env.BaseHostPublic = pubHost
+	}
+
+	privHost := os.Getenv("INDODAX_PRIV_HOST")
+	if privHost != "" {
+		env.BaseHostPrivate = privHost
+	}
+
+	return env
+}
