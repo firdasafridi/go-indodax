e7053d2accbd6bde79c8818bfc473773b7afdbb3
firdasafridi <firdasafridi@ymail.com>
2019-07-14 19:33:16 +0700

    Add get response from orderbook

--- public.go
+++ public.go
@@ -7,14 +7,14 @@ import (
 
 func (cl *Client) GetTicker(pairName string) (ticker *Ticker, err error) {
 	if pairName == "" {
-		return ticker, ErrInvalidPairName
+		return nil, ErrInvalidPairName
 	}
 
 	urlPath := fmt.Sprintf(pathTicker, pairName)
 
 	body, err := cl.curlPublic(urlPath)
 	if err != nil {
-		return ticker, fmt.Errorf("GetTicker: " + err.Error())
+		return nil, fmt.Errorf("GetTicker: " + err.Error())
 	}
 
 	printDebug(string(body))
@@ -22,13 +22,36 @@ func (cl *Client) GetTicker(pairName string) (ticker *Ticker, err error) {
 	tickerRes := tickerResponse{}
 	err = json.Unmarshal(body, &tickerRes)
 	if err != nil {
-		return ticker, fmt.Errorf("GetTicker: " + err.Error())
+		return nil, fmt.Errorf("GetTicker: " + err.Error())
 	}
 
 	ticker, err = tickerRes.toTicker(pairName)
 	if err != nil {
-		return ticker, fmt.Errorf("GetTicker: " + err.Error())
+		return nil, fmt.Errorf("GetTicker: " + err.Error())
 	}
 
 	return ticker, nil
+}
+
+func (cl *Client) GetOrderBook(pairName string) (orderBook *OrderBook, err error) {
+	if pairName == "" {
+		return nil, ErrInvalidPairName
+	}
+
+	urlPath := fmt.Sprintf(pathDepth, pairName)
+
+	body, err := cl.curlPublic(urlPath)
+	if err != nil {
+		return nil, fmt.Errorf("GetOrderBook: " + err.Error())
+	}
+
+	printDebug(string(body))
+
+	orderBook = &OrderBook{}
+	err = json.Unmarshal(body, &orderBook)
+	if err != nil {
+		return nil, fmt.Errorf("GetOrderBook: " + err.Error())
+	}
+
+	return orderBook, nil
 }
