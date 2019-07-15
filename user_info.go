0b6f57810d96b318f188a973164d0af9f32b76b6
firdasafridi <firdasafridi@ymail.com>
2019-07-15 14:00:50 +0700

    Private API GetInfo

---
+++ user_info.go
@@ -0,0 +1,70 @@
+package indodax
+
+import (
+	"fmt"
+	"time"
+	"encoding/json"
+	"strconv"
+	"strings"
+)
+
+type respGetInfo struct {
+	Success		int
+	Return	*UserInfo
+}
+
+type UserInfo struct {
+	Balance			map[string]float64
+	BalanceHold		map[string]float64
+	WalletAddress	map[string]string
+	UserId			int
+	ProfilePicture	string
+	UserName		string
+	ServerTime		time.Time
+	Email			string
+}
+
+func (UserInfo	*UserInfo) UnmarshalJSON(b []byte) (err error) {
+	var kv map[string]interface{}
+
+	err = json.Unmarshal(b, &kv)
+	if err != nil {
+		return err
+	}
+
+	for k, v := range kv {
+		k = strings.ToLower(k)
+		
+		switch k {
+		case fieldNameBalance:
+			UserInfo.Balance, err = jsonToMapStringFloat64(v.(map[string]interface{}))
+		case fieldNameBalanceHold:
+			UserInfo.BalanceHold, err = jsonToMapStringFloat64(v.(map[string]interface{}))
+		case fieldNameWalletAddress:
+			UserInfo.WalletAddress, err = jsonToMapStringString(v.(map[string]interface{}))
+		case fieldNameUserId:
+			val64, err := strconv.Atoi(v.(string))
+			if err != nil {
+				return err
+			}
+			UserInfo.UserId = int(val64)
+		case fieldNameProfilePicture:
+			UserInfo.ProfilePicture = fmt.Sprintf("%v",v)
+		case fieldNameEmail:
+			UserInfo.Email = fmt.Sprintf("%v",v)
+		case fieldNameUserName:
+			UserInfo.UserName = fmt.Sprintf("%v",v)
+		case fieldNameUserServerTime:
+			ts, err := strconv.ParseInt(fmt.Sprintf("%.0f",v), 10, 64)
+			if err != nil {
+				return err
+			}
+			UserInfo.ServerTime = time.Unix(ts, 0)
+		}
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
